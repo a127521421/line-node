9cdb68721b89e6746116319fec26bc107eddd834
a127521421 <a127521421@yahoo.com.tw>
2020-05-05 16:13:50 +0800

    新增:顯示:職業、攻擊、血量，

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import dotenv from 'dotenv'
 // 引用 request 套件
 import rp from 'request-promise'
 
+// https://zerozeroone.herokuapp.com/
+
 // 讀取.env檔
 dotenv.config()
 
@@ -28,12 +30,25 @@ const delHtmlTag = (str) => {
 
 bot.on('message', async (event) => {
   let msg = ''
+  const r = '無'
   try {
     // const data = await rp({ uri: 'https://kktix.com/events.json', json: true })
     const data = await rp({ uri: 'https://api.hearthstonejson.com/v1/45932/zhTW/cards.collectible.json', json: true })
     for (let i = 0; i <= 2536; i++) {
-      if (event.message.text === data[i].name) {
-        msg = `花費:${data[i].cost}\n趣味說明: ${data[i].flavor}\n效果:${delHtmlTag(data[i].text)}`
+      if (event.message.text === data[i].name && data[i].cost !== undefined) {
+        msg = ([
+          {
+            type: 'text',
+            text: `花費:${data[i].cost}\n職業:${data[i].cardClass}\n攻擊:${data[i].attack !== undefined ? data[i].attack : r
+              }\n血量:${data[i].health !== undefined ? data[i].health : r} \n效果: ${delHtmlTag(data[i].text)}\n趣味說明: ${data[i].flavor} `
+          },
+          { type: 'image', originalContentUrl: `https://art.hearthstonejson.com/v1/render/latest/zhTW/256x/${data[i].id}.png`, previewImageUrl: `https://art.hearthstonejson.com/v1/render/latest/zhTW/256x/${data[i].id}.png` }
+        ])
+        break
+      } else if (event.message.text === '使用方法') {
+        msg = '請輸入卡片名稱\nPS:有些卡片的名字需要輸入『』'
+      } else {
+        msg = '請輸入正確的名稱'
       }
     }
   } catch (error) {
